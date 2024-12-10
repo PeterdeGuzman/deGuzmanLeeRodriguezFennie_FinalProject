38fde639516d3fcf91801d09bb17e676244dd4cd
Eric Ortega Rodriguez <ericiortega85@gmail.com>
2024-12-10 18:26:51 +0000

    load

---
+++ load_test.py
@@ -0,0 +1,23 @@
+from locust import HttpUser, task, between
+
+class FlaskAppLoadTest(HttpUser):
+    # Simulate user wait time between tasks (1 to 3 seconds)
+    wait_time = between(1, 3)
+
+    @task
+    def home_page(self):
+        # Test the homepage
+        self.client.get("/")
+
+    @task
+    def project_page(self):
+        # Test the project page
+        self.client.get("/project")
+
+    @task
+    def submit_query(self):
+        # Test the submit route with a POST request
+        self.client.post(
+            "/submit"
+            data={"userInput": "Ernest Hemingway", "options": "Author"}
+        )
